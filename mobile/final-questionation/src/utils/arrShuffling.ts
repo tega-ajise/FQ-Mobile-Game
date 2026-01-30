6348fa9e2ac71d4413689f86dfcb25ab96440632
Tega Ajise <tegaajise@Tegas-MacBook-Pro.local>
2026-01-30 13:34:51 -0500

    shuffling array algorithm

---
+++ mobile/final-questionation/src/utils/arrShuffling.ts
@@ -0,0 +1,16 @@
+// src/utils/arrShuffling.ts
+
+/**
+ * Shuffle an array using Fisher-Yates algorithm.
+ * @param arr - input array
+ * @param inPlace - if true, shuffle the input array; otherwise return a shuffled copy
+ * @returns shuffled array
+ */
+export function shuffle<T>(arr: T[], inPlace = false): T[] {
+  const a = inPlace ? arr : arr.slice();
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
