311c474ec81a6e4609300660c24a484032f67218
Tega Ajise <tegaajise@Tegas-MacBook-Pro.local>
2026-02-20 18:40:32 -0500

    SSL connection for socket established

--- mobile/final-questionation/src/hooks/AppProvider.tsx
+++ mobile/final-questionation/src/hooks/AppProvider.tsx
@@ -33,21 +33,8 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
 
     const socket = io(url, {
       reconnectionDelayMax: 10000,
-      transports: ['websocket'], // just for verifying server connection - REMOVE
-      upgrade: false, // just for verifying server connection - DO NOT KEEP THIS
     });
     socketRef.current = socket;
-
-    socket.on('connect_error', (err) => {
-      // the reason of the error, for example "xhr poll error"
-      console.log(err.message);
-
-      // some additional description, for example the status code of the initial HTTP response
-      console.log(err.description);
-
-      // some additional context, for example the XMLHttpRequest object
-      console.log(err.context);
-    });
 
     socket.on('initLobbies', (allLobbies: LobbyDetails[]) => {
       setLobbies(allLobbies);
